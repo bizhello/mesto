5d17b4f6583995dc5ce2c823c422d06c8390252e
Andrey Bizhov <bijev14@yandex.ru>
2022-03-03 21:35:00 +0300

    mini

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,6 @@
 let profileEdit = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
-
-function togglePopup(){
-  popup.classList.toggle('popup-opened');
-}
-
-profileEdit.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup);
-
 let profile = document.querySelector('.profile');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
@@ -16,11 +8,16 @@ let popupButton = document.querySelector('.popup__button');
 let popupStatus = document.querySelector('#popup-status');
 let popupName = document.querySelector('#popup-name');
 
+function togglePopup(){
+  popup.classList.toggle('popup-opened');
+}
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileStatus.textContent = popupStatus.value;
 }
 
+profileEdit.addEventListener('click', togglePopup);
+popupClose.addEventListener('click', togglePopup);
 popupButton.addEventListener('click', formSubmitHandler);
 popupButton.addEventListener('click', togglePopup);
