207a84990b58fcfd1901e50697530af092aa076d
Andrey Bizhov <bijev14@yandex.ru>
2022-03-04 02:25:51 +0300

    pc1

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,24 @@ let popupClose = document.querySelector('.popup__close');
 let profile = document.querySelector('.profile');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
-let popupButton = document.querySelector('.popup__button');
+let popupContainer = document.querySelector('.popup__container');
 let popupStatus = document.querySelector('#popup-status');
 let popupName = document.querySelector('#popup-name');
 
-function togglePopup(){
-  popup.classList.toggle('popup-opened');
-}
+function openPopup() {
+  popup.classList.add('popup_opened');
+}//открытие попапа
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}//закрытие попапа
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileStatus.textContent = popupStatus.value;
-}
+    closePopup();
+}//смена имени и статуса
 
-profileEdit.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup);
-popupButton.addEventListener('click', formSubmitHandler);
-popupButton.addEventListener('click', togglePopup);
+profileEdit.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+popupContainer.addEventListener('submit', formSubmitHandler );
